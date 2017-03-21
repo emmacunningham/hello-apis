647d751b80eed8caba707e36086112f9b9cded24
Emma Cunningham <emma@secondspectrum.com>
2017-03-21 05:47:14 -0700

    Add giphy example

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function pageReady() {
 
   // This code makes an HTTP GET REQUEST to the OMDb API
   // the RESPONSE represents the data from OMDb
-  $.get( "http://www.omdbapi.com/?s=samurai", function( response ) {
+  $.get( "http://www.omdbapi.com/?s=" + searchInput, function( response ) {
     console.log(response);
   });
 
@@ -16,5 +16,11 @@ function pageReady() {
   });
 
 }
+
+// This code will make an HTTP GET REQUEST to the Giphy API searching the cats tag
+// We're not doing anything with the response right now, but try using console.log!
+$.get( "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats", function(response) {
+
+});
 
 $(document).ready(pageReady);
