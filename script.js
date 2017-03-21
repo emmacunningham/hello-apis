ded73636ac8bdd47fb5b76eebc22cf665b84eabb
Emma Cunningham <emma@secondspectrum.com>
2017-03-21 05:35:57 -0700

    Lol remembering to save is a good thing

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 // Code executed in this function will run when the page is ready
 function pageReady() {
 
+  // This code makes an HTTP GET REQUEST to the OMDb API
+  // the RESPONSE represents the data from OMDb
   $.get( "http://www.omdbapi.com/?s=samurai", function( response ) {
     console.log(response);
   });
 
+  // This code sets up our search button so that when we click it,
+  // we get whatever the user typed in the input box
+  $('.search-button').click(function() {
+    var searchInput = $('.search-input').val();
+    console.log(searchInput);
+  });
 
 }
-
 
 $(document).ready(pageReady);
